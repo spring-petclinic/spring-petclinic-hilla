2106a44208e0afaa2754b1c5c7758e18bd8a2636
Kari Söderholm <haprog@vaadin.com>
2021-09-16 15:55:34 +0300

    Add a Vaadin Fusion based hello-world view and theme

---
+++ frontend/routes.ts
@@ -0,0 +1,32 @@
+import { Route } from '@vaadin/router';
+import './views/helloworld/hello-world-view';
+import './views/main-layout';
+
+export type ViewRoute = Route & {
+    title?: string;
+    icon?: string;
+    children?: ViewRoute[];
+};
+
+export const views: ViewRoute[] = [
+    // place routes below (more info https://vaadin.com/docs/latest/fusion/routing/overview)
+    {
+        path: '',
+        component: 'hello-world-view',
+        icon: '',
+        title: '',
+    },
+    {
+        path: 'hello',
+        component: 'hello-world-view',
+        icon: 'la la-globe',
+        title: 'Hello World',
+    },
+];
+export const routes: ViewRoute[] = [
+    {
+        path: '',
+        component: 'main-layout',
+        children: [...views],
+    },
+];
