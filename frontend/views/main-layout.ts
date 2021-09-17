bb7af257aae2eec93b3bb142b92cef40db0162b5
Kari Söderholm <haprog@vaadin.com>
2021-09-17 16:23:52 +0300

    Use top navigation in Fusion views
    Update main-layout.ts to have the navigation menu at the top instead of on the side. Based on top menu option in start.vaadin.com.

--- frontend/views/main-layout.ts
+++ frontend/views/main-layout.ts
@@ -1,11 +1,8 @@
 import '@vaadin/vaadin-app-layout';
-import { AppLayoutElement } from '@vaadin/vaadin-app-layout';
-import '@vaadin/vaadin-app-layout/vaadin-drawer-toggle';
 import '@vaadin/vaadin-avatar/vaadin-avatar';
 import '@vaadin/vaadin-context-menu';
 import '@vaadin/vaadin-tabs';
 import '@vaadin/vaadin-tabs/vaadin-tab';
-import '@vaadin/vaadin-template-renderer';
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { router } from '../index';
@@ -23,37 +20,34 @@ interface RouteInfo {
 export class MainLayout extends Layout {
   render() {
     return html`
-      <vaadin-app-layout primary-section="drawer">
-        <header class="bg-base border-b border-contrast-10 box-border flex h-xl items-center w-full" slot="navbar">
-          <vaadin-drawer-toggle aria-label="Menu toggle" class="text-secondary" theme="contrast"></vaadin-drawer-toggle>
-          <h1 class="m-0 text-l">${appStore.currentViewTitle}</h1>
-        </header>
-        <section class="flex flex-col items-stretch max-h-full min-h-full" slot="drawer">
-          <h2 class="flex items-center h-xl m-0 px-m text-m">${appStore.applicationName}</h2>
-          <nav aria-labelledby="views-title" class="border-b border-contrast-10 flex-grow overflow-auto">
-            <h3 class="flex items-center h-m mx-m my-0 text-s text-tertiary" id="views-title">Views</h3>
-              <a class="flex mx-s p-s relative text-secondary"
-                href="/"
-                router-ignore
-              >
-                <span class="me-s text-l"></span>
-                <span class="font-medium text-s">Original Home</span>
-              </a>
+      <vaadin-app-layout>
+        <header class="bg-base border-b border-contrast-10 box-border flex flex-col w-full" slot="navbar">
+          <div class="flex h-xl items-center px-l">
+            <h1 class="my-0 me-auto text-l">${appStore.applicationName}</h1>
+          </div>
+          <nav class="flex gap-s overflow-auto px-m">
+            <a class="flex mx-s p-s relative text-secondary"
+              href="/"
+              router-ignore
+            >
+              <span class="me-s text-l"></span>
+              <span class="font-medium text-s">Original Home</span>
+            </a>
             ${this.getMenuRoutes().map(
-            (viewRoute) => html`
-              <a
-                ?highlight=${viewRoute.path == appStore.location}
-                class="flex mx-s p-s relative text-secondary"
-                href=${router.urlForPath(viewRoute.path)}
-              >
-                <span class="${viewRoute.icon} me-s text-l"></span>
-                <span class="font-medium text-s">${viewRoute.title}</span>
-              </a>
-            `
-        )}
+      (viewRoute) => html`
+                <a
+                  ?highlight=${viewRoute.path == appStore.location}
+                  class="flex 
+                  h-m items-center px-s relative text-secondary"
+                  href=${router.urlForPath(viewRoute.path)}
+                >
+                  <span class="${viewRoute.icon} me-s text-l"></span>
+                  <span class="font-medium text-s whitespace-nowrap">${viewRoute.title}</span>
+                </a>
+              `
+    )}
           </nav>
-          <footer class="flex items-center my-s px-m py-xs"></footer>
-        </section>
+        </header>
         <slot></slot>
       </vaadin-app-layout>
     `;
@@ -62,12 +56,6 @@ export class MainLayout extends Layout {
   connectedCallback() {
     super.connectedCallback();
     this.classList.add('block', 'h-full');
-    this.reaction(
-      () => appStore.location,
-      () => {
-        AppLayoutElement.dispatchCloseOverlayDrawerEvent();
-      }
-    );
   }
 
   private getMenuRoutes(): RouteInfo[] {
