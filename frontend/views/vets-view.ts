25cf407193c4827668a6e76194a67df22234d322
Kari Söderholm <haprog@vaadin.com>
2021-09-24 13:29:15 +0300

    Update VetsView to use vaadin-grid with sorting

--- frontend/views/vets-view.ts
+++ frontend/views/vets-view.ts
@@ -1,49 +1,65 @@
-import { html } from 'lit';
+import { html, HTMLTemplateResult, nothing, render } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
+import '@vaadin/vaadin-grid/vaadin-grid';
+import type { GridBodyRenderer } from '@vaadin/vaadin-grid';
+import '@vaadin/vaadin-grid/vaadin-grid-column';
+import '@vaadin/vaadin-grid/vaadin-grid-sort-column';
 import { View } from '../views/view';
 import Vet
   from 'Frontend/generated/org/springframework/samples/petclinic/vet/Vet';
 import { VetEndpoint } from 'Frontend/generated/endpoints';
 
+interface VetGridItem extends Vet {
+  fullNameForSorting: string;
+}
+
 @customElement('vets-view')
 export class VetsView extends View {
   @state()
-  vets: Readonly<Array<Vet>> = [];
+  vetsGridItems: Array<VetGridItem> = [];
+
+  private nameRenderer: GridBodyRenderer<Vet> = (root, _, model) => {
+    const vet = model.item;
+    render(html`${vet.firstName} ${vet.lastName}`, root);
+  }
+
+  private specialtiesRenderer: GridBodyRenderer<Vet> = (root, _, model) => {
+    const vet = model.item;
+    const templateJoin = (values: Array<HTMLTemplateResult>, joiner: HTMLTemplateResult) => values.map((v, i) => [v, i < values.length - 1 ? joiner : nothing]);
+    render(
+      (vet.specialties.length > 0)
+        ? templateJoin(
+          vet.specialties.map(
+            (specialty) => html`<span>${specialty.name}</span>`
+          ),
+          html`, ` // Add a comma between the items
+        )
+        : html`<span>none</span>`,
+      root
+    );
+  }
 
   constructor() {
     super();
-    VetEndpoint.list().then((vets) => this.vets = vets);
+    VetEndpoint.list().then(
+      (vets) =>
+        this.vetsGridItems = vets.map(
+          (vet) => ({
+            ...vet,
+            fullNameForSorting: `${vet.lastName} ${vet.firstName}`,
+          })
+        )
+    );
   }
 
   render() {
     return html`
       <h2>Veterinarians</h2>
       
-      <table id="vets" class="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Specialties</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${this.vets.map(
-            (vet) => html`
-              <tr>
-                <td>${vet.firstName} ${vet.lastName}</td>
-                <td>
-                  ${(vet.specialties.length > 0)
-                    ? vet.specialties.map(
-                      (specialty) => html`
-                      <span>${specialty.name} </span>
-                    `)
-                    : html`<span>none</span>`}
-                </td>
-              </tr>
-            `
-          )}
-        </tbody>
-      </table>
+      <vaadin-grid .items="${this.vetsGridItems}" theme="row-stripes" all-rows-visible>
+        <vaadin-grid-sort-column header="Name" path="fullNameForSorting" .renderer="${this.nameRenderer}"></vaadin-grid-sort-column>
+        <vaadin-grid-column header="Specialties" .renderer="${this.specialtiesRenderer}"></vaadin-grid-column>
+      </vaadin-grid>
     `;
   }
 }
