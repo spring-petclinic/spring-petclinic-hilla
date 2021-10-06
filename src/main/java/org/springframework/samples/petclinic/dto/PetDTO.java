c261b8d6b768cf42999f89e3c5cb6f749c548f78
Kari Söderholm <haprog@vaadin.com>
2021-10-06 15:15:09 +0300

    Add server side validation to PetDTO

--- src/main/java/org/springframework/samples/petclinic/dto/PetDTO.java
+++ src/main/java/org/springframework/samples/petclinic/dto/PetDTO.java
@@ -1,5 +1,8 @@
 package org.springframework.samples.petclinic.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 import com.vaadin.fusion.Nonnull;
@@ -9,12 +12,16 @@ public class PetDTO {
 	private Integer id;
 
 	@Nonnull
+	@NotBlank(message = "must not be empty")
 	private String name;
 
 	@Nonnull
+	@NotNull(message = "must not be empty")
+	@PastOrPresent
 	private LocalDate birthDate;
 
 	@Nonnull
+	@NotNull(message = "type must be selected")
 	private Integer typeId;
 
 	@Nonnull
