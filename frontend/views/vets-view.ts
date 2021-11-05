a7be3a391a61fbf0bdecf0c2adbb1fdfe3fee68c
Marcus Hellberg <marcus@vaadin.com>
2021-11-05 09:58:18 -0700

    remove special sorting as the original didn't have it either

--- frontend/views/vets-view.ts
+++ frontend/views/vets-view.ts
@@ -1,56 +1,51 @@
 import { html, render } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import '@vaadin/grid';
-import type { GridBodyRenderer } from '@vaadin/grid';
 import '@vaadin/grid/vaadin-grid-column';
 import '@vaadin/grid/vaadin-grid-sort-column';
+import type { GridBodyRenderer } from '@vaadin/grid';
 import { View } from '../views/view';
-import Vet
-  from 'Frontend/generated/org/springframework/samples/petclinic/vet/Vet';
+import Vet from 'Frontend/generated/org/springframework/samples/petclinic/vet/Vet';
 import { VetEndpoint } from 'Frontend/generated/endpoints';
-
-interface VetGridItem extends Vet {
-  fullNameForSorting: string;
-}
 
 @customElement('vets-view')
 export class VetsView extends View {
   @state()
-  vetsGridItems: VetGridItem[] = [];
+  vets: Vet[] = [];
 
   private nameRenderer: GridBodyRenderer<Vet> = (root, _, model) => {
     const vet = model.item;
     render(html`${vet.firstName} ${vet.lastName}`, root);
-  }
+  };
 
   private specialtiesRenderer: GridBodyRenderer<Vet> = (root, _, model) => {
     const vet = model.item;
     render(
-      (vet.specialties.length > 0)
-        ? html`<span>${vet.specialties.map((specialty) => specialty.name).join(', ')}</span>`
+      vet.specialties.length > 0
+        ? html`<span>
+            ${vet.specialties.map((specialty) => specialty.name).join(', ')}
+          </span>`
         : html`<span>none</span>`,
       root
     );
-  }
+  };
 
   async connectedCallback() {
     super.connectedCallback();
-    const vets = await VetEndpoint.list();
-    this.vetsGridItems = vets.map(
-      (vet) => ({
-        ...vet,
-        fullNameForSorting: `${vet.lastName} ${vet.firstName}`,
-      })
-    )
+    this.vets = await VetEndpoint.list();
   }
 
   render() {
     return html`
       <h2>Veterinarians</h2>
-      
-      <vaadin-grid .items="${this.vetsGridItems}" theme="row-stripes" all-rows-visible>
-        <vaadin-grid-sort-column header="Name" path="fullNameForSorting" .renderer="${this.nameRenderer}"></vaadin-grid-sort-column>
-        <vaadin-grid-column header="Specialties" .renderer="${this.specialtiesRenderer}"></vaadin-grid-column>
+
+      <vaadin-grid .items="${this.vets}" theme="row-stripes" all-rows-visible>
+        <vaadin-grid-sort-column
+          header="Name"
+          .renderer="${this.nameRenderer}"></vaadin-grid-sort-column>
+        <vaadin-grid-column
+          header="Specialties"
+          .renderer="${this.specialtiesRenderer}"></vaadin-grid-column>
       </vaadin-grid>
     `;
   }
